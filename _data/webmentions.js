916ba36bab7a71e048663d83c29e2104f1af2932
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-07-14 22:25:31 +0200

    Add tag for name and show name in replied

--- _data/webmentions.js
+++ _data/webmentions.js
@@ -1,4 +1,5 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
+const Parser = require("microformats-parser");
 
 // data/webmentions.js
 const API_ORIGIN = 'https://webmention.io/api/mentions.jf2'
@@ -9,7 +10,18 @@ module.exports = async function () {
     const url = `${API_ORIGIN}?domain=${domain}&token=${token}`
 
     try {
-        const response = await EleventyFetch(url, {type: 'json'})
+        const response = await EleventyFetch(url, {type: 'json', duration: "0s"})
+        let promises = response.children.map(async (child, index) => {
+            if(child["in-reply-to"]){
+                const string = await EleventyFetch(child["in-reply-to"], {type: "text", duration: "0s"});
+                let parsed = Parser.mf2(string, {baseUrl: child["in-reply-to"]})
+                response.children[index].replyInfo = parsed;
+                return response.children
+            }
+            return response.children
+        })
+        let newChildren = await Promise.all(promises)
+        response.children = newChildren.flat()
         return response
     } catch (err) {
         console.error(err)
