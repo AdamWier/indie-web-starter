df7ee12220c332e0734f35e61be62d070819025e
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-07-20 01:33:36 +0200

    Add mastodon feed

---
+++ _data/mastodon.js
@@ -0,0 +1,13 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+
+module.exports = async function () {
+    const url = `https://mastodon.social/api/v1/accounts/112156626614796336/statuses`
+
+    try {
+        const statuses = await EleventyFetch(url, {type: 'json', duration: "0s"})
+        return statuses.filter(status => status.content && !status.content.includes('@'))
+    } catch (err) {
+        console.error(err)
+        return null
+    }
+}
