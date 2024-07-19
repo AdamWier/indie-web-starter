69d2783ee0d54c11ba0622a9cc3a39305766801e
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-07-19 18:59:43 +0200

    Revert "Add daisyUI"
    This reverts commit 5a02e8e2ccccc916d3f46f0e62b6b491841ff9fd.

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,25 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["_site/**/*.html"],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("daisyui")],
-	daisyui: {
-		themes: [
-			{
-				mytheme: {
-					primary: "#d300ff",
-					secondary: "#42cd00",
-					accent: "#00a0ff",
-					neutral: "#0f0d1b",
-					"base-100": "#e8fdff",
-					info: "#00d3ff",
-					success: "#00f4ad",
-					warning: "#e10000",
-					error: "#ef0d5a",
-				},
-			},
-		],
-	},
-};
+  content: ['_site/**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
