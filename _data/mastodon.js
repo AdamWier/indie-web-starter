3d1d6d666d5b842920c78eb8cbfa547377b05242
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-07-20 10:39:59 +0200

    No reblogs

--- _data/mastodon.js
+++ _data/mastodon.js
@@ -1,7 +1,7 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-    const url = `https://mastodon.social/api/v1/accounts/112156626614796336/statuses`
+    const url = `https://mastodon.social/api/v1/accounts/112156626614796336/statuses?exclude_reblogs=true`
 
     try {
         const statuses = await EleventyFetch(url, {type: 'json', duration: "0s"})
