6b9b1fbf9d7a412606d2aed655b5dfd2611fd6e1
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-07-20 22:36:28 +0200

    Currently reading

---
+++ _data/reading.js
@@ -0,0 +1,20 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+const Image = require("@11ty/eleventy-img");
+
+module.exports = async function () {
+    const base = "https://openlibrary.org";
+    const url = `${base}/people/donthatedontkill1811/books/currently-reading.json`
+
+    try {
+        const response = await EleventyFetch(url, {type: 'json', duration: "0s"})
+        let key = response.reading_log_entries[0].work.key
+        let book = await EleventyFetch(base+key+'.json', {type: 'json', duration: "0s"})
+        return {
+            ...book,
+            author: response.reading_log_entries[0].work.author_names[0]
+        }
+    } catch (err) {
+        console.error(err)
+        return null
+    }
+}
