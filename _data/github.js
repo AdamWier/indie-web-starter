0ae3e55f9d02c67469e829dfd24e3cd6f889b00a
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-07-24 18:44:17 +0200

    Add latest commits and fav repos

--- _data/github.js
+++ _data/github.js
@@ -1,16 +1,17 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-    const url = `https://api.github.com/users/adamwier/repos`
+    const eventsUrl = `https://api.github.com/users/adamwier/events`;
+    const userUrl = "https://api.github.com/users/adamwier";
 
     try {
-        const response = await EleventyFetch(url, {type: 'json', duration: "0s"})
-        const latestUpdated = response.sort((a,b) => new Date(b.updated_at) - new Date(a.updated_at))[0]
-        const commits = await EleventyFetch(latestUpdated.commits_url.split('{')[0], {type: 'json', duration: "0s"})
-        return {
-            commit: commits[0],
-            repostiory: latestUpdated,
-        }
+        const response = await EleventyFetch(eventsUrl, {type: 'json', duration: "0s"})
+        const myPushEvents = response.filter(event => event.type === "PushEvent" && event.payload.commits[0].author.name === "AdamWier");
+        const gitHubActivity = myPushEvents.map(event => ({
+            message: event.payload.commits[0].message,
+            commitUrl: event.payload.commits[0].url 
+        }));
+        return gitHubActivity;
     } catch (err) {
         console.error(err)
         return null
