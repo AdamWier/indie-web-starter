80231a9ab15bf51dd04d6169c794afe3c69e52a2
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-08-16 12:49:37 +0200

    Fix bad json

--- eleventy.config.js
+++ eleventy.config.js
@@ -103,8 +103,6 @@ module.exports = function(eleventyConfig) {
 
 	eleventyConfig.addFilter('removeHtml', (string) => stripHtml(string))
 
-	eleventyConfig.addFilter('replaceLineBreaks', str => str.split(/\r?\n|\r/).join('\n\n'))
-
 	eleventyConfig.addFilter('tagify', tags => tags.map(tag => `#${tag}`).join(' '));
 
 	// Customize Markdown library settings:
