b07d5c8cf8b9a1b3052cfc0948ec876cffa0533c
Adam Wier <wier.adam@gmail.com>
2024-10-14 19:34:55 +0200

    Fix reply to webmentions not showing right

--- _data/webmentions.js
+++ _data/webmentions.js
@@ -16,14 +16,13 @@ const verb = {
 
 const fillInTemplate = (child) => `
     <img src="${child.author?.photo}" class="img-fluid update-avatar-photo" /> 
-    <a href="${child.author?.url}">
-        ${child.author?.name}
-    </a> 
-    liked 
-    <a href="${child["like-of"]}">
-        ${child["like-of"]}
-    </a>`;
-
+    <a href="${child.author?.url}">${child.author?.name}</a> ${verb[child["wm-property"]]}
+    <a href="${child[child["wm-property"]]}">
+        ${child[child["wm-property"]]} 
+    </a>
+    <br/>
+    ${child.content?.html || child.content?.text || ""}
+    `;
 module.exports = async function () {
     const domain = 'mycabinetofcuriosities.com'
     const token = process.env.WEBMENTION_TOKEN
@@ -31,18 +30,7 @@ module.exports = async function () {
 
     try {
         const response = await EleventyFetch(url, {type: 'json', duration: "0s"})
-        // let promises = response.children.map(async (child, index) => {
-        //     if(child["in-reply-to"]){
-        //         const string = await EleventyFetch(child["in-reply-to"], {type: "text", duration: "0s"});
-        //         let parsed = Parser.mf2(string, {baseUrl: child["in-reply-to"]})
-        //         response.children[index].replyInfo = parsed;
-        //         return response.children
-        //     }
-        //     return response.children
-        // })
-        // let newChildren = await Promise.all(promises)
-        // response.children = newChildren.flat()
-        return response.children.map(child =>({
+        return response.children.map(child =>(console.log(child.content, child.author, child) || {
             ...child,
             statement: `${child.author.name} ${verb[child["wm-property"]]} ${child[child["wm-property"]]}`,
             content: child.content?.html || child["like-of"] ? fillInTemplate(child) : `Web mention from ${child.author?.name}`,
