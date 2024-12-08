1398025a46edeab005287feb86895db140ed3592
Adam Wier <wier.adam@gmail.com>
2024-12-08 18:08:55 +0100

    Restore pulling current book from open library

--- _data/reading.js
+++ _data/reading.js
@@ -1,20 +1,20 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-    // const base = "https://openlibrary.org";
-    // const url = `${base}/people/donthatedontkill1811/books/currently-reading.json`
+    const base = "https://openlibrary.org";
+    const url = `${base}/people/donthatedontkill1811/books/currently-reading.json`
 
-    // try {
-    //     const response = await EleventyFetch(url, {type: 'json', duration: "0s"})
-    //     let key = response.reading_log_entries[0].work.key
-    //     let book = await EleventyFetch(base+key+'.json', {type: 'json', duration: "0s"})
-    //     return {
-    //         ...book,
-    //         key,
-    //         author: response.reading_log_entries[0].work.author_names[0]
-    //     }
-    // } catch (err) {
-    //     console.error(err)
-    //     return null
-    // }
+    try {
+        const response = await EleventyFetch(url, {type: 'json', duration: "0s"})
+        let key = response.reading_log_entries[0].work.key
+        let book = await EleventyFetch(base+key+'.json', {type: 'json', duration: "0s"})
+        return {
+            ...book,
+            key,
+            author: response.reading_log_entries[0].work.author_names[0]
+        }
+    } catch (err) {
+        console.error(err)
+        return null
+    }
 }
